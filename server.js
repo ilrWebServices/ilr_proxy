598d83c21b64aea9310fae3b8a8e6d363f235077
Jeff Amaral <jeff.amaral@cornell.edu>
2019-11-21 10:50:55 -0800

    Update D8 source url and add /programs/professional-education path

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var server = http.createServer(function(req, res) {
   let proxy_opts = {};
 
   // D8 site.
-  // if (req.url.startsWith('/programs/professional-programs') || req.url.startsWith('/core') || req.url.startsWith('/libraries/union') || req.url.startsWith('/themes/custom/union_marketing') || req.url.startsWith('/sites/default/files-d8')) {
-  //   proxy_opts.target = 'https://d8.master-7rqtwti-yf4o2w34wqxm6.us-2.platformsh.site';
+  // if (req.url.startsWith('/programs/professional-education') || req.url.startsWith('/programs/professional-programs') || req.url.startsWith('/core') || req.url.startsWith('/libraries/union') || req.url.startsWith('/themes/custom/union_marketing') || req.url.startsWith('/sites/default/files-d8')) {
+  //   proxy_opts.target = 'https://d8.ilr.cornell.edu';
   // }
 
   // Add a header to the response to help with debugging the source of this
