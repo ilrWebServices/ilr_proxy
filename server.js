92ca2efd8b2c69425af01eb94561b76865eb027b
Jeff Amaral <jeff.amaral@cornell.edu>
2019-12-16 13:40:24 -0800

    Refactor proxy to run D8 checks as a separate test

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const http = require('http'),
     httpProxy = require('http-proxy'),
+    url = require('url'),
     dotenv = require('dotenv').config();
 
-var proxy = httpProxy.createProxyServer({
-  target: 'https://www.master-7rqtwti-dd2imk5jkez6q.us-2.platformsh.site',
+const drupal_7_url = 'https://www.master-7rqtwti-dd2imk5jkez6q.us-2.platformsh.site',
+      drupal_8_url = 'https://d8.ilr.cornell.edu';
+
+const should_use_d8 = (req) => {
+  const d8_path_prefixes = [
+          '/programs/professional-education',
+          '/programs/professional-programs',
+          '/core',
+          '/libraries/union',
+          '/themes/custom/union_marketing',
+          '/sites/default/files-d8',
+          '/media/oembed'
+        ];
+
+  // Test the incoming request against the above path prefixes. This will be set
+  // to true if the incoming request URL starts with any of the prefixes above.
+  const d8_path_prefix_test = d8_path_prefixes.some((path_prefix) => req.url.startsWith(path_prefix));
+
+  return d8_path_prefix_test;
+};
+
+// Create the proxy with a default target of the D7 site.
+const proxy = httpProxy.createProxyServer({
+  target: drupal_7_url,
   changeOrigin: true,
   autoRewrite: true,
   preserveHeaderKeyCase: true,
@@ -11,12 +34,13 @@ var proxy = httpProxy.createProxyServer({
   cookieDomainRewrite: ""
 });
 
-var server = http.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
   let proxy_opts = {};
 
-  // D8 site.
-  if (req.url.startsWith('/programs/professional-education') || req.url.startsWith('/programs/professional-programs') || req.url.startsWith('/core') || req.url.startsWith('/libraries/union') || req.url.startsWith('/themes/custom/union_marketing') || req.url.startsWith('/sites/default/files-d8') || req.url.startsWith('/media/oembed')) {
-    proxy_opts.target = 'https://d8.ilr.cornell.edu';
+  // Proxy this request to D8 if the incoming request passes certain criteria.
+  // This is done by overriding the default proxy target, set above.
+  if (should_use_d8(req)) {
+    proxy_opts.target = drupal_8_url;
   }
 
   // Add a header to the response to help with debugging the source of this
