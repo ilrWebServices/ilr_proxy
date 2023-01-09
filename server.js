98fc9d5d8af31b418a100c267039adfef695e387
Jeff Amaral <jeff.amaral@cornell.edu>
2023-01-09 12:24:02 -0500

    Add a redirect for /eform/submit/transfer-offer
    This will become a redirect on the D9 site

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const should_use_d8 = (req) => {
     '/coronavirus',
     '/course',
     '/diversity-equity-and-inclusion',
+    '/eform/submit/transfer-offer',
     '/expo',
     '/global-labor-institute',
     '/hooks/v1',
