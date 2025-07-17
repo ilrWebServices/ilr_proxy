880e5e658f37537f3ac19b3e4481684a251eed52
Jeff Amaral <jeff.amaral@cornell.edu>
2025-07-17 16:34:40 -0400

    Reapply "Add /ncrs to drupal-latest"
    This reverts commit d0e84eef6c00f479efb0412fd30ac578d2978c0e.

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ const should_use_d8 = (req) => {
     '/media/oembed',
     '/modules/contrib',
     '/modules/custom',
+    '/ncrs',
     '/new-conversations-project',
     '/new-york-city',
     '/news',
